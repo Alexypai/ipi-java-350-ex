77821c4c5fb8f0f39a27cdd18cb1ab1462ac0ef6
Alexy <alexy.paiva@ipilyon.net>
2021-02-10 19:02:37 +0100

    Ajout de test EmployeServiceTest

---
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -0,0 +1,51 @@
+package com.ipiecoles.java.java350.service;
+
+import com.ipiecoles.java.java350.exception.EmployeException;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.NiveauEtude;
+import com.ipiecoles.java.java350.model.Poste;
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeServiceTest {
+
+    @InjectMocks
+    private EmployeService employeService;
+
+    @Mock
+    private EmployeRepository employeRepository;
+
+    @Test
+    public void testEmbauchePremierEmploye() throws EmployeException {
+        //Given Pas d'employés en base
+        String nom = "Doe";
+        String prenom = "John";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
+        Mockito.when(employeRepository.save(Mockito.any(Employe.class))).thenAnswer(AdditionalAnswers.returnsFirstArg());
+        //When
+        Employe employe = employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+        //Then
+//        Employe employe = employeRepository.findByMatricule("T00001");
+        Assertions.assertThat(employe).isNotNull();
+        Assertions.assertThat(employe.getNom()).isEqualTo(nom);
+        Assertions.assertThat(employe.getPrenom()).isEqualTo(prenom);
+        Assertions.assertThat(employe.getSalaire()).isEqualTo(1825.464);
+        Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(1.0);
+        Assertions.assertThat(employe.getDateEmbauche()).isEqualTo(LocalDate.now());
+        Assertions.assertThat(employe.getMatricule()).isEqualTo("T00001");
+    }
+}
