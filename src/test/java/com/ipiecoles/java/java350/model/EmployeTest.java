540174b6a700ccdf09b78df779a118e673e4c0a1
Alexy <alexy.paiva@ipilyon.net>
2021-02-12 17:12:06 +0100

    //Nouveau commit pour mettre a jour la branche SonarCloud

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -10,6 +10,8 @@ import java.time.LocalDate;
 
 public class EmployeTest {
 
+    //Nouveau commit pour mettre a jour la branche SonarCloud
+
     @Test
     public void testGetAnneeAcienneteDateEmbaucheNull(){
         //GIVEN
